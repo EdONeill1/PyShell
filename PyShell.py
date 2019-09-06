446e5e56e82ef5afde15f68144f3c719cc0d2dc0
EdONeill1 <edward.oneill@ucdconnect.ie>
2019-09-06 22:14:53 +0100

    Working ' > ' redirect

--- PyShell.py
+++ PyShell.py
@@ -4,6 +4,8 @@ import string
 import re
 import signal
 
+
+
 def parseString(input):
         i = 0
         tokens = list()
@@ -11,18 +13,39 @@ def parseString(input):
         executeCommand(tokens)
 
 def changeDirectory(tokens):
-        if sys.getsizeof(tokens) == 1:
-                os.chdir("..")
-                exit(0)
-        else:
                 os.chdir(tokens[1])
                 exit(0)
+
+def redirects(tokens, redirectPoint):
+        redirectTokens = list()
+        for i in range(redirectPoint):
+                redirectTokens.append(tokens[i])
+        filename = tokens[len(tokens) - 1]
+        child_process = os.fork()
+        if(child_process == 0):
+                 fd = os.open(filename, os.O_WRONLY|os.O_CREAT|os.O_TRUNC);
+                 os.dup2(fd, 1)
+                 os.close(fd)
+                 os.execvp(redirectTokens[0], redirectTokens)
+        elif child_process < 0:
+                print("Error")
+
+        else:
+                os.wait()
+        
 
 def executeCommand(tokens):
 
         if(tokens[0] == "cd"):
                 changeDirectory(tokens)
         
+
+        redir1 = ">"
+        for index in range(len(tokens)):
+                if tokens[index] == redir1:
+                        redirects(tokens, index)
+                        return
+
         pid = os.fork()
         if pid == 0:
                 os.execvp(tokens[0], tokens)
